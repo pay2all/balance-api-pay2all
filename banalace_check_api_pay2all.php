aa594962ec96209a3c4e363157f13b82d28515a4
sanjay live <i@sanjay.live>
2017-10-25 12:59:52 +0530

    Update

--- banalace_check_api_pay2all.php
+++ banalace_check_api_pay2all.php
@@ -9,9 +9,9 @@
 
         $url = "https://www.pay2all.in/api/dmr/v1/balance";
 
-        $key = "eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImp0aSI6IjI0NTAxOWU2YTg0ZmU4OTUxOTMxMmQwY2ZmNDIzZGFmNTc0MWYwYTFhYjQxZTFhOTFjZThjZjljNmIxNDQxMzY3ZGNmZWFjNzhkNmE3ODY3In0.eyJhdWQiOiI1IiwianRpIjoiMjQ1MDE5ZTZhODRmZTg5NTE5MzEyZDBjZmY0MjNkYWY1NzQxZjBhMWFiNDFlMWE5MWNlOGNmOWM2YjE0NDEzNjdkY2ZlYWM3OGQ2YTc4NjciLCJpYXQiOjE0OTI5ODExNTgsIm5iZiI6MTQ5Mjk4MTE1OCwiZXhwIjoxNTI0NTE3MTU4LCJzdWIiOiI5NDkiLCJzY29wZXMiOltdfQ.jbz46sl1xkP_0w7ww43x5EmPmZBbU6Gs0fjA4SU42YtvLCJd0k-IMJuP-UuEF88CJfu1ZvYEBI3uypPifIQ2Jm0Ht3ZQdQkO2xdf9ayKiRRqGxYwSINHuiGVRuFxtmcr6L7a6AmwR-s1-7MLRiF_uJet10PQXP2MLT3zOoTJpcDj1eSg-ktPtGpSzU4qPzkmgf5SXxoZDq2o7_C-A9kAaMWsnKZ6MwBpeOkROgx591gKO4bR2OyiE3vye9DGItL6EkgPuuw_3_B4qeoAI6sQkEsJWEoeU6L3RuaIiLL3JlpveACIUlnn8HGbGt6dqhe-IK52vY485HTaPa5OJMrMZ1ImNuLW0uja78rKROB2y7I-TgtUfT5QeE7K6xffeP36xSusC_2r1d9uborKYlrC_GBXI82aXLzkk1j6SIfTHPI4wuCIaOwqtR_WAo6j8gpZKOf02qeVqw239KXbTDq53NDJXjffXRwn6xqUjG6XJ_FFYhQt6VXSEoR8uRG_PiHr4-YdFTlgtBIVg6GzjwEjbvpj41NuI0XN2qMlH6jGi2DMm3HOR3AvPtsI5iQgvPARYamOqSjaLMjuZ1TgcKB941Bi9BMkTRDx0qYhYuRL90oxpK3YJPdj587cobye79wcSm31ihPfRzeC4xwQkCofZHk2Xtu1GkYpX5-hHCcFUWg"; //you have to add personal access token 
+        $personal_access_token = ""; //you have to add personal access token 
 
-        $header = ["Accept:application/json", "Authorization:Bearer ".$key];
+        $header = ["Accept:application/json", "Authorization:Bearer ".$personal_access_token];
 
         $method = 'POST';
 
